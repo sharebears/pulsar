7c99f550e3fda4f1ae8d7f680ef2253be1f34efa
lights <lights@tutanota.de>
2018-10-10 18:35:50 -0400

    initialize basic pulsar glue site

---
+++ pulsar/__init__.py
@@ -0,0 +1,17 @@
+import flask
+import core
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+migrate = Migrate()
+
+
+def create_app(config: str) -> flask.Flask:
+    app = flask.Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    app.config.from_pyfile(config)
+
+    db = core.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
