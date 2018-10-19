a5f562fd5651669483a6d15950b7881740c3aead
lights <lights@tutanota.de>
2018-10-19 00:39:30 -0400

    add more commands to the developer db manipulations

--- pulsar/dev.py
+++ pulsar/dev.py
@@ -5,13 +5,19 @@ import click
 import flask
 from flask.cli import with_appcontext
 
+import sqlalchemy
 from core import cache, db
+from core.users.models import User
 from core.test_data import CorePopulator
+from forums.test_data import ForumsPopulator
+from messages.test_data import MessagesPopulator
 
 app = flask.current_app
 
 POPULATORS = [
     CorePopulator,
+    ForumsPopulator,
+    MessagesPopulator,
     ]
 
 
@@ -23,10 +29,29 @@ def dev():
 
 @dev.command()
 @with_appcontext
+def createdb():
+    """Create an empty database per the current schema."""
+    db.create_all()
+
+
+@dev.command()
+@with_appcontext
 def createdata():
+    """Recreate and repopulate an existing testing database."""
     with app.test_request_context():
         flask.g.cache_keys = defaultdict(set)
-        db.drop_all()
+        try:
+            user = User.from_pk(1)
+        except sqlalchemy.exc.ProgrammingError as e:
+            click.secho(
+                'Please manually create the database before running the createdata command.',
+                fg='red', bold=True)
+            raise click.Abort
+        if user and user.username != 'user_one':
+            click.secho('The username of the first user is not user_one. Are you sure this '
+                        'is a testing database? Please clear it yourself and re-try.',
+                        fg='red', bold=True)
+            raise click.Abort
         db.create_all()
         for p in POPULATORS:
             p.populate()
